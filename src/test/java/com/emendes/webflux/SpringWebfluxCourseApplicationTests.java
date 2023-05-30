a019b108c807be6ca226198a93e187f7eca11f9f
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-05-30 14:38:31 -0300

    first commit

---
+++ src/test/java/com/emendes/webflux/SpringWebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emendes.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
